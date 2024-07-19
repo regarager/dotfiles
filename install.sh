a8a02e04a7eff27da971cca05d6b0b3ee4680ef5
regarager <redgerxu@gmail.com>
2024-07-19 11:07:42 -0700

    updated install file and changed opacity

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 pacman -S git stow
 pacman -S alacritty nvim
-pacman -S exa bat
+pacman -S exa bat yay
 pacman -S hyprland hyprpaper brightnessctl hyprshot xdg-desktop-portal-gtk xdg-desktop-portal-hyprland waybar wofi network-manager-applet
+pacman -S ttf-jetbrains-mono
 pacman -S vesktop
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
