21deecf645abe07d8e12a3c0bc123e8a2f10c694
regarager <redgerxu@gmail.com>
2024-07-31 11:24:57 -0700

    changed font and battery format

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ pacman -S git stow
 pacman -S alacritty nvim
 pacman -S exa bat yay
 pacman -S hyprland hyprpaper brightnessctl hyprshot xdg-desktop-portal-gtk xdg-desktop-portal-hyprland waybar wofi network-manager-applet
-pacman -S ttf-jetbrains-mono
+pacman -S ttf-0xproto
 pacman -S vesktop
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
