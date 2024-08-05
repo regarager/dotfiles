8420e769c8778f18f5f734ae8dd34b5f3885dbf9
regarager <redgerxu@gmail.com>
2024-08-04 18:55:48 -0700

    alacritty and install.sh changes

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
-pacman -S git stow
-pacman -S alacritty nvim
-pacman -S exa bat yay
-pacman -S hyprland hyprpaper brightnessctl hyprshot xdg-desktop-portal-gtk xdg-desktop-portal-hyprland waybar wofi network-manager-applet
-pacman -S ttf-0xproto
-pacman -S vesktop
+pacman -S git stow yay
+pacman -S alacritty neovim
+pacman -S exa bat
+yay -S hyprland hyprpaper brightnessctl hyprshot xdg-desktop-portal-gtk xdg-desktop-portal-hyprland waybar wofi network-manager-applet
+yay -S ttf-jetbrains-mono
+yay -S vesktop
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone https://github.com/alacritty/alacritty-theme .config/alacritty/themes
 
 stow --adopt .
+
+nvim -c "PackerInstall"
