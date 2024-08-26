9bcc4379628059af1cca8053a86c0bee75533a61
regarager <redgerxu@gmail.com>
2024-08-25 19:37:16 -0700

    updated install.sh to not use packer

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ yay -S stow alacritty neovim \
     ttf-jetbrains-mono ttf-jetbrains-mono-nerd \
     vesktop
 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 git clone https://github.com/alacritty/alacritty-theme .config/alacritty/themes
 
 stow --adopt .
 
-nvim -c "PackerInstall"
+echo "Run 'nvim' to get start automatic setup for Neovim"
