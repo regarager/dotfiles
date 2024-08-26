9b82c56364c4765b42f197ac5ee7a2c960b9013e
regarager <redgerxu@gmail.com>
2024-08-26 15:04:45 -0700

    nvim updates

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,27 +1,27 @@
 return {
     -- Syntax, LSP, etc.
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }, -- Treesitter
+    { "nvim-treesitter/nvim-treesitter",  build = ":TSUpdate" }, -- Treesitter
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim" },
     { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp" }, -- Autocomplete
+    { "hrsh7th/nvim-cmp" },           -- Autocomplete
     { "hrsh7th/cmp-nvim-lsp" },
-    { "onsails/lspkind.nvim" }, -- Autocomplete icons
-    { "mfussenegger/nvim-dap" }, -- Debug Adapter Protocol
+    { "onsails/lspkind.nvim" },       -- Autocomplete icons
+    { "mfussenegger/nvim-dap" },      -- Debug Adapter Protocol
     { "mhartington/formatter.nvim" }, -- Formatting
     { "VonHeikemen/lsp-zero.nvim" },
-    { "jiangmiao/auto-pairs" }, -- Auto pairs
-    { "windwp/nvim-ts-autotag" }, -- Autotag
+    { "jiangmiao/auto-pairs" },       -- Auto pairs
+    { "windwp/nvim-ts-autotag" },     -- Autotag
     {
-        "L3MON4D3/LuaSnip", -- Snippet engine
-        version = "v2.*", -- Use the latest release version
+        "L3MON4D3/LuaSnip",           -- Snippet engine
+        version = "v2.*",             -- Use the latest release version
         build = "make install_jsregexp"
     },
     -- Files
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" }
     },
     {
         "nvim-neo-tree/neo-tree.nvim",
@@ -30,7 +30,7 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
-            "lewis6991/gitsigns.nvim" -- Git status (optional)
+            "lewis6991/gitsigns.nvim"      -- Git status (optional)
         }
     },
     { "romgrk/barbar.nvim" }, -- Tabs
@@ -39,10 +39,10 @@ return {
     { "olimorris/onedarkpro.nvim" },
     { "xiyaowong/transparent.nvim" }, -- Transparent background
     -- Language Plugins
-    { "fatih/vim-go" }, -- Go
-    { "ziglang/zig.vim" }, -- Zig
-    { "rust-lang/rust.vim" }, -- Rust
-    { "nvim-java/nvim-java" }, -- Java
+    { "fatih/vim-go" },               -- Go
+    { "ziglang/zig.vim" },            -- Zig
+    { "rust-lang/rust.vim" },         -- Rust
+    { "nvim-java/nvim-java" },        -- Java
     -- Misc.
     {
         "folke/which-key.nvim",
@@ -57,7 +57,7 @@ return {
                 desc = "Buffer Local Keymaps (which-key)",
             },
         },
-    }, -- which-key
-    { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } } -- Lualine
+    },                                                                                 -- which-key
+    { "nvim-lualine/lualine.nvim",    dependencies = { "nvim-tree/nvim-web-devicons" } }, -- Lualine
+    { dir = "~/Projects/lua/jclasses" }, --        dependencies = { "MunifTanjim/nui.nvim" } }
 }
-
