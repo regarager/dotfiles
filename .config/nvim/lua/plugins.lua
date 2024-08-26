7eb76f9717fe62f5d9082c81b3d94eab7b6e0880
regarager <redgerxu@gmail.com>
2024-08-25 20:03:41 -0700

    added telescope keymap and reformatted plugins file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,45 +1,20 @@
 return {
     -- Syntax, LSP, etc.
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = { ":TSUpdate" }
-    }, -- Treesitter
-    {
-        "williamboman/mason.nvim"
-    },
-    {
-        "williamboman/mason-lspconfig.nvim"
-    },
-    {
-        "neovim/nvim-lspconfig"
-    },
-    {
-        "hrsh7th/nvim-cmp" -- Autocomplete
-    },
-    {
-        "mfussenegger/nvim-dap" -- Debug Adapter Protocol
-    },
-    {
-        "hrsh7th/cmp-nvim-lsp"
-    },
-    {
-        "onsails/lspkind.nvim" -- Autocomplete icons
-    },
-    {
-        "mhartington/formatter.nvim" -- Formatting
-    },
-    {
-        "VonHeikemen/lsp-zero.nvim"
-    },
-    {
-        "jiangmiao/auto-pairs" -- Auto pairs
-    },
-    {
-        "windwp/nvim-ts-autotag" -- Autotag
-    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }, -- Treesitter
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/nvim-cmp" }, -- Autocomplete
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "onsails/lspkind.nvim" }, -- Autocomplete icons
+    { "mfussenegger/nvim-dap" }, -- Debug Adapter Protocol
+    { "mhartington/formatter.nvim" }, -- Formatting
+    { "VonHeikemen/lsp-zero.nvim" },
+    { "jiangmiao/auto-pairs" }, -- Auto pairs
+    { "windwp/nvim-ts-autotag" }, -- Autotag
     {
         "L3MON4D3/LuaSnip", -- Snippet engine
-        version = "v2.*",   -- Use the latest release version
+        version = "v2.*", -- Use the latest release version
         build = "make install_jsregexp"
     },
     -- Files
@@ -55,39 +30,34 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
-            "lewis6991/gitsigns.nvim"      -- Git status (optional)
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+            "lewis6991/gitsigns.nvim" -- Git status (optional)
         }
     },
-    {
-        "romgrk/barbar.nvim" -- Tabs
-    },
-    -- Lualine
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
+    { "romgrk/barbar.nvim" }, -- Tabs
     -- Themes/Appearance
-    {
-        "rebelot/kanagawa.nvim"
-    },
-    {
-        "olimorris/onedarkpro.nvim"
-    },
-    {
-        "xiyaowong/transparent.nvim" -- Transparent background
-    },
+    { "rebelot/kanagawa.nvim" },
+    { "olimorris/onedarkpro.nvim" },
+    { "xiyaowong/transparent.nvim" }, -- Transparent background
     -- Language Plugins
+    { "fatih/vim-go" }, -- Go
+    { "ziglang/zig.vim" }, -- Zig
+    { "rust-lang/rust.vim" }, -- Rust
+    { "nvim-java/nvim-java" }, -- Java
+    -- Misc.
     {
-        "fatih/vim-go" -- Go
-    },
-    {
-        "ziglang/zig.vim" -- Zig
-    },
-    {
-        "rust-lang/rust.vim" -- Rust
-    },
-    {
-        "nvim-java/nvim-java" -- Java
-    }
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    }, -- which-key
+    { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } } -- Lualine
 }
+
