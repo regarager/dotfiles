8654c2bf7d80bd8843b70599cde930b8ef18a646
Redger Xu <redgerxu@gmail.com>
2024-09-15 22:18:01 -0700

    added polkit install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ yay -S zsh stow alacritty neovim \
     jenv jdk8-openjdk jdk11-openjdk jdk17-openjdk jdk-openjdk \
     rust \
     eza bat fastfetch fzf zoxide less net-tools github-cli-git battop bashtop thefuck \
-    hyprland hyprpaper hyprshot hyprlock dunst brightnessctl xdg-desktop-portal-gtk xdg-desktop-portal-hyprland waybar wofi breeze breeze-gtk qt6ct \
+    hyprland hyprpaper hyprshot hyprlock dunst brightnessctl xdg-desktop-portal-gtk xdg-desktop-portal-hyprland waybar wofi breeze breeze-gtk qt6ct qt5ct polkit-kde-agent \
     ttf-jetbrains-mono-nerd ttf-space-mono-nerd ttf-comic-shanns-nerd ttf-0xproto-nerd nodejs unzip \
     vesktop
 
