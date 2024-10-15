f3f0d86bd2afb1f5cb6b664436063eaa23eaa14d
Redger Xu <redgerxu@gmail.com>
2024-10-15 16:12:53 -0700

    fixed line number colors

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -26,9 +26,9 @@ opt.clipboard:append("unnamedplus")
 opt.splitright = true
 opt.splitbelow = true
 
+vim.cmd.colorscheme("tokyonight-moon")
+vim.cmd("TransparentEnable")
+
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#999999" })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#e6e6e6" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#999999" })
-
-vim.cmd.colorscheme("tokyonight-moon")
-vim.cmd("TransparentEnable")
