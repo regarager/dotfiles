daf4be5fb77f2354f112c649f100bb6919c0ded2
Redger Xu <redgerxu@gmail.com>
2024-10-31 19:49:26 -0700

    added toggleterm

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -4,8 +4,22 @@ local keymap = vim.keymap
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<leader>v", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>h", "<C-w>s", { desc = "Split window horizontaly" })
+keymap.set("n", "<leader>h", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Set windows to equal size" })
 keymap.set("n", "<leader>q", ":close<CR>", { desc = "Close current window" })
 
+keymap.set("n", "<leader>t", ":ToggleTerm<CR>", { desc = "Toggle terminal" })
+
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 keymap.set("n", "\\", ":Neotree toggle<cr>", { desc = "Toggle Neotree" })
