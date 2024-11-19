4bc7063372ce9a594dc1f06d85b0a8f6929d7d16
Redger Xu <redgerxu@gmail.com>
2024-11-18 21:17:08 -0800

    java go byebye

--- .config/nvim/lua/plugins/langs.lua
+++ .config/nvim/lua/plugins/langs.lua
@@ -2,11 +2,4 @@ return {
   "ziglang/zig.vim",
   "fatih/vim-go",
   "regarager/jclasses",
-  {
-    "nvim-java/nvim-java",
-    config = function()
-      require('java').setup()
-      require('lspconfig').jdtls.setup({})
-    end
-  }
 }
