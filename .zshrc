eeb50927ef3fc7452e31515509dee88823bdc2de
Redger Xu <redgerxu@gmail.com>
2025-03-30 19:06:34 -0700

    updated prompt

--- .zshrc
+++ .zshrc
@@ -2,10 +2,10 @@
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 # if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+#    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 # fi
 
-source ~/zsh-prompt/prompt.zsh
+source ~/shiko-prompt/prompt.zsh
 
 # Lines configured by zsh-newuser-install
 HISTFILE=~/.histfile
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 yay -S zsh stow alacritty ghostty neovim zen-browser-bin \
     jenv jdk8-openjdk jdk11-openjdk jdk17-openjdk jdk-openjdk npm nodejs rust \
-    eza bat fastfetch fzf unzip ripgrep grep git-delta zoxide zsh-theme-powerlevel10k-git zsh-syntax-highlighting less net-tools github-cli-git battop bashtop thefuck \
+    eza bat fastfetch fzf unzip ripgrep grep git-delta zoxide zsh-syntax-highlighting less net-tools github-cli-git battop bashtop thefuck \
     hyprland hypridle hyprpaper hyprshot hyprlock mako \
     brightnessctl xdg-desktop-portal-gtk xdg-desktop-portal-hyprland waybar \
-    wofi breeze breeze-gtk qt6ct qt5ct polkit-gnome xorg-xhost iwgtk \
+    wofi breeze breeze-gtk qt6ct qt5ct polkit-gnome xorg-xhost iwgtk iwd networkmanager network-manager-applet \
     bluez bluez-utils overskride \
     ttf-jetbrains-mono-nerd ttf-space-mono-nerd otf-comicshanns-nerd ttf-0xproto-nerd noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
     vesktop
@@ -16,7 +16,7 @@ echo "Finished copying dotfiles"
 
 git clone https://github.com/alacritty/alacritty-theme .config/alacritty/themes
 git clone https://github.com/Aloxaf/fzf-tab ~
-git clone https://github.com/regarager/zsh-prompt ~
+git clone https://github.com/regarager/shiko-prompt ~
 
 echo "Setting up git-delta options"
 git config --global core.pager delta
