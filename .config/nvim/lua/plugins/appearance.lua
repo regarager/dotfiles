772738106aedcaa5457266e0097312a70e30a32a
Redger Xu <redgerxu@gmail.com>
2025-04-17 23:13:24 -0700

    updated lualine config

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -1,13 +1,5 @@
 return {
 	"folke/tokyonight.nvim",
-	-- { "rose-pine/neovim", name = "rose-pine" },
+	{ "rose-pine/neovim", name = "rose-pine" },
 	"rebelot/kanagawa.nvim",
-	-- "olimorris/onedarkpro.nvim",
-	-- "EdenEast/nightfox.nvim",
-	-- "sainnhe/everforest",
-	-- "catppuccin/nvim",
-	-- "ribru17/bamboo.nvim",
-	-- "dasupradyumna/midnight.nvim",
-	-- "AlexvZyl/nordic.nvim",
-	-- "sam4llis/nvim-tundra",
 }
