ed26d53797d950a89ddcdcfe8b5e86bfb4f1872c
Redger Xu <redgerxu@gmail.com>
2025-05-02 19:53:54 -0700

    deleted alacritty, update to duskfox

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -1,4 +1,9 @@
 return {
 	"folke/tokyonight.nvim",
 	"Mofiqul/dracula.nvim",
+	"rebelot/kanagawa.nvim",
+	"EdenEast/nightfox.nvim",
+	"dgox16/oldworld.nvim",
+	"mellow-theme/mellow.nvim",
+	{ "rose-pine/neovim", name = "rose-pine" },
 }
--- .config/nvim/lua/plugins/lines.lua
+++ .config/nvim/lua/plugins/lines.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			require("lualine").setup({
 				options = {
-					theme = "palenight",
+					theme = "horizon",
 					component_separators = "",
 					section_separators = { left = "", right = "" },
 				},
