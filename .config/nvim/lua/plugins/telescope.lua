6fba800b9e508757100dd3a9f04767cdb200031b
Redger Xu <redgerxu@gmail.com>
2025-05-24 15:17:35 -0700

    fzf on telescope, updated tmux style

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,77 +1,71 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"andrew-george/telescope-themes",
-		},
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					file_ignore_patterns = {
-						"node_modules",
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "andrew-george/telescope-themes",
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}
+    },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules",
             ".git"
-					},
-					results_title = false,
-					sorting_strategy = "ascending",
-					layout_strategy = "center",
-					layout_config = {
-						preview_cutoff = 1, -- Preview should always show (unless previewer = false)
-						width = function(_, max_columns, _)
-							return math.min(max_columns, 80)
-						end,
-						height = function(_, _, max_lines)
-							return math.min(max_lines, 15)
-						end,
-					},
-					border = true,
-					borderchars = {
-						prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
-						results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
-						preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-					},
-				},
-				pickers = {
-					find_files = { follow = true, hidden = true},
-				},
+          },
+          results_title = false,
+          sorting_strategy = "ascending",
+          layout_strategy = "center",
+          layout_config = {
+            preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+            width = function(_, max_columns, _)
+              return math.min(max_columns, 80)
+            end,
+            height = function(_, _, max_lines)
+              return math.min(max_lines, 15)
+            end,
+          },
+          border = true,
+          borderchars = {
+            prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+            results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+          },
+        },
+        pickers = {
+          find_files = { follow = true, hidden = true},
+        },
 
-				extensions = {
-					themes = {
-						persist = { enable = false },
-					},
-				},
-			})
+        extensions = {
+          themes = {
+            persist = { enable = false },
+          },
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          }
+        },
+      })
 
-			local telescope = require("telescope")
-			telescope.load_extension("themes")
+      require("telescope").load_extension("themes")
+      require("telescope").load_extension('fzf')
 
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "ff", builtin.find_files, {})
-			vim.keymap.set("n", "fg", builtin.live_grep, {})
-			vim.keymap.set("n", "fb", builtin.buffers, {})
-			vim.keymap.set("n", "fh", builtin.help_tags, {})
-			vim.keymap.set("n", "fc", builtin.current_buffer_fuzzy_find, {})
-			vim.keymap.set("n", "fs", builtin.lsp_workspace_symbols, {})
-			vim.keymap.set(
-				"n",
-				"fth",
-				":Telescope themes<CR>",
-				{ noremap = true, silent = true, desc = "Theme Switcher" }
-			)
-		end,
-	},
-	{
-		"ibhagwan/fzf-lua",
-		-- optional for icon support
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			-- calling `setup` is optional for customization
-			require("fzf-lua").setup({
-				file_ignore_patterns = {
-					"node_modules",
-				},
-			})
-		end,
-	},
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "ff", builtin.find_files, {})
+      vim.keymap.set("n", "fg", builtin.live_grep, {})
+      vim.keymap.set("n", "fb", builtin.buffers, {})
+      vim.keymap.set("n", "fh", builtin.help_tags, {})
+      vim.keymap.set("n", "fc", builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set("n", "fs", builtin.lsp_workspace_symbols, {})
+      vim.keymap.set(
+        "n",
+        "fth",
+        ":Telescope themes<CR>",
+        { noremap = true, silent = true, desc = "Theme Switcher" }
+      )
+    end,
+  },
 }
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ yay -S --noconfirm --needed \
   zsh stow ghostty neovim zen-browser-bin \
   eza bat fastfetch fzf unzip ripgrep grep git-delta zoxide thefuck less net-tools tree-sitter tree-sitter-cli \
   zsh-syntax-highlighting github-cli battop btop cpupower \
-  jenv jdk8-openjdk jdk11-openjdk jdk17-openjdk jdk-openjdk npm nodejs rust go astyle \
+  jenv jdk8-openjdk jdk11-openjdk jdk17-openjdk jdk-openjdk npm nodejs rust go astyle luarocks \
   hyprland hypridle hyprpaper grimblast hyprlock mako ly waybar \
   brightnessctl xdg-desktop-portal-gtk xdg-desktop-portal-hyprland \
   wofi breeze breeze-gtk qt6ct qt5ct polkit-gnome xorg-xhost iwgtk iwd networkmanager network-manager-applet \
