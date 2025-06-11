dacb1935c53a73b863620c855fac186e5386c992
Redger Xu <redgerxu@gmail.com>
2025-06-11 15:40:53 -0700

    idk what happened in lspconfig but new astyle rules

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,33 +1,57 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
 
-		require("mason-lspconfig").setup()
+    require("mason-lspconfig").setup()
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-			callback = function(ev)
-				local opts = { buffer = ev.buf, silent = true }
-			end,
-		})
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        local opts = { buffer = ev.buf, silent = true }
+      end,
+    })
 
-		lspconfig["tinymist"].setup({
-			settings = {
-				formatterMode = "typstyle",
-				exportPdf = "onSave",
-			},
-		})
+    lspconfig["tinymist"].setup({
+      settings = {
+        formatterMode = "typstyle",
+        exportPdf = "onSave",
+      },
+    })
 
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "javascriptreact,typescriptreact",
-			callback = function()
-				vim.b.disable_react_tag_warning = true
-			end,
-		})
-	end,
+    lspconfig["lua_ls"].setup({
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+          runtime = {
+            version = 'LuaJIT'
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+            }
+          }
+        })
+      end,
+      settings = {
+        Lua = {}
+      }
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "javascriptreact,typescriptreact",
+      callback = function()
+        vim.b.disable_react_tag_warning = true
+      end,
+    })
+  end,
 }
